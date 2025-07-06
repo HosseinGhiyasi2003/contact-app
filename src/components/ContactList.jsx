01004c9d7111b1e6dbd78323d9baae7ca381d20a
Hossein Ghiyasi <hosseinghiyasi2020@gmail.com>
2025-07-06 11:49:44 +0330

    completed: search bar

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,18 +1,42 @@
+import { useEffect, useState } from "react";
 import ContactItem from "./ContactItem";
 import SearchBar from "./SearchBar";
 
 function ContactList({ setCurrentView, contacts, setContacts, setSelectedContactId }) {
+
+   const [searchInput, setSearchInput] = useState("") 
+   const [searchTerm, setSearchTerm] = useState('')
+
   const deleteContactHandler = (id) => {
     setContacts((contacts) => contacts.filter((contact) => contact.id !== id));
   };
 
- 
+  useEffect(() => {
+  const delay = setTimeout(() => {
+    setSearchTerm(searchInput.toLowerCase());
+  }, 400); // تاخیر ۴۰۰ میلی‌ثانیه
+
+  return () => clearTimeout(delay); // اگه کاربر دوباره تایپ کرد، تایمر قبلی پاک شه
+}, [searchInput]);
+
+  const normalize = (str) =>
+  str?.toLowerCase().trim().replace(/\s+/g, "");
+
+
+  const filteredContacts = contacts.filter((contact) => {
+  const fullName = `${contact.name} ${contact.lastName}`;
+  return (
+    normalize(fullName).includes(normalize(searchTerm)) ||
+    normalize(contact.email).includes(normalize(searchTerm))
+  );
+});
+
 
   return (
     <div className="w-screen h-screen bg-[#d5eeff] flex justify-center items-center">
       <div className="w-96 bg-[#fffefd] mx-auto px-5 py-6 rounded-2xl">
         <h2 className="text-xl font-semibold">Contact List</h2>
-        <SearchBar />
+        <SearchBar searchTerm={searchInput} searchChangeHandle={(e) => setSearchInput(e.target.value)} />
         <div className="flex justify-between items-center mt-2.5 mb-3">
           <button className="bg-[#ddf7f5] border-3 border-[#c4eff0] text-[#1b7162] font-medium rounded-[10px] w-34 h-10 cursor-pointer">
             Delete Selected
@@ -25,9 +49,9 @@ function ContactList({ setCurrentView, contacts, setContacts, setSelectedContact
           </button>
         </div>
         <div className="max-h-52 overflow-y-auto">
-          {contacts.length ? (
+          {filteredContacts.length ? (
             <>
-              {contacts.map((contact) => (
+              {filteredContacts.map((contact) => (
                 <ContactItem
                   key={contact.id}
                   {...contact}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,13 @@
-function SearchBar() {
+function SearchBar({searchChangeHandle, searchTerm}) {
+
+
   return (
     <div className="w-full bg-[#fffaef] border-2 border-[#fedfb5] rounded-[10px] px-3 py-2 flex mt-3">
       &#128269;
       <input
         type="text"
+        onChange={searchChangeHandle}
+        value={searchTerm}
         placeholder="Search by name or email"
         className="ml-2.5 w-full border-0 outline-0"
       />
